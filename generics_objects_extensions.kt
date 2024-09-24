968f917ca3552543627a795cd630d75836426192
shogo.takasaki <stakasaki707@gmail.com>
2024-09-25 01:04:05 +0900

    rewrite extension functions using interfaces

--- generics_objects_extensions.kt
+++ generics_objects_extensions.kt
@@ -15,10 +15,21 @@ data class Question<T>(
 )
 
 
-class Quiz{
+class Quiz : ProgressPrintable{
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("#") }
+        repeat(Quiz.total - Quiz.answered) { print("*") }
+        println()
+        println(progressText)
+    }
+    
 
     companion object StudentProgress {
         var total: Int = 10
@@ -26,16 +37,11 @@ class Quiz{
     }
 }
 
-val Quiz.StudentProgress.progressText: String
-    get() = "${answered} of ${total} answered"
-
-fun Quiz.StudentProgress.printProgressBar() {
-    repeat(Quiz.answered) { print("#") }
-    repeat(Quiz.total - Quiz.answered) { print("*") }
-    println()
-    println(Quiz.progressText)
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
 }
 
  fun main() {
-    Quiz.printProgressBar()
+    Quiz().printProgressBar()
 }
