e9d4d3fedde617e9b1d6371203d519d157d11cb9
shogo.takasaki <stakasaki707@gmail.com>
2024-09-25 00:50:07 +0900

    use a singleton object

--- generics_objects_extensions.kt
+++ generics_objects_extensions.kt
@@ -14,9 +14,18 @@ data class Question<T>(
     val difficulty: Difficulty
 )
 
- fun main() {
+
+class Quiz{
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-    println(question1.toString())
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+ fun main() {
+    println("${Quiz.answered} of ${Quiz.total} answered.")
 }
