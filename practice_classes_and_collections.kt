2dc12307dac70721c4c7d0d80bec0b978b8f2b76
shogo.takasaki <stakasaki707@gmail.com>
2024-09-28 14:58:32 +0900

    task3

--- practice_classes_and_collections.kt
+++ practice_classes_and_collections.kt
@@ -6,7 +6,7 @@
     val title: String,
     val description: String?,
     val dayPart: DayPart,
-    val duration: Int
+    val durationInMinutes: Int
  )
 
  enum class DayPart {
@@ -16,11 +16,12 @@
  }
 
  fun main() {
-    val event = Event(
-        title = "Study Kotlin",
-        description = "Commit to studying Kotlin at least 15 minutes per day.",
-        dayPart = DayPart.EVENING,
-        duration = 15
+    val events = mutableListOf(
+        Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0),
+        Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15),
+        Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30),
+        Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60),
+        Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10),
+        Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
     )
-    println(event)
  }
